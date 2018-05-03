6c6112aa858ca33ec693b18a5fedd5b946c35004
Daniel Brunelle <daniel.brunelle@me.com>
2018-05-03 15:42:21 -0600

    Made link code more modular + added link state machine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,46 +5,105 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-
 	public int speed;
 	private Animator anim;
-	private SpriteRenderer sr;
-	float animspeed;
 
-	Dictionary<string, Vector2> directions;
-	Dictionary<string, int> movement;
+	//States associated with each type of movement
+	private struct MoveState
+	{
+		public string name;
+		public KeyCode[] keys;
+		public Vector2 direction;
+		public bool flip;
+
+	}
+	private SpriteRenderer sr;
+	private float animspeed;
+	private enum State { Idle, Moving, Attacking, Frozen };
+	private State linkState;
+
+	/*
+	 * All possible abbreviated movements
+	 * ur = Up Right
+	 * ul = Up Left
+	 * dr = Down Right
+	 * dl = Down Left
+	 * rt = Right
+	 * lt = Left
+	 * up = Up
+	 * dn = Down
+	 */
+	private string[] moveDirs = new string[] { "ur", "ul", "dr", "dl", "rt", "lt", "up", "dn" };
+
+	private Dictionary<string, MoveState> moveDict;
 
 
 	// Use this for initialization
 	void Start()
 	{
+		linkState = State.Idle;
 		anim = GetComponent<Animator>();
 		sr = GetComponent<SpriteRenderer>();
 		anim.speed = 1;
 
-		//Associate direction with its vector value
-		directions = new Dictionary<string, Vector2>()
-		{
-			{"up", Vector2.up},
-			{"down", Vector2.down},
-			{"left", Vector2.left},
-			{"right", Vector2.right},
-			{"upRight", new Vector2(1,1).normalized},
-			{"upLeft", new Vector2(-1,1).normalized},
-			{"downRight", new Vector2(1,-1).normalized},
-			{"downLeft", new Vector2(-1,-1).normalized},
-		};
-
-		movement = new Dictionary<string, int>()
-		{
-			{"up", 1},
-			{"down", 2},
-			{"left", 0},
-			{"right", 0},
-			{"upRight", 3},
-			{"upLeft", 3},
-			{"downRight",4},
-			{"downLeft", 4},
+		//Define states for each type of movement
+		moveDict = new Dictionary<string, MoveState>(){
+			//Up Right
+			{moveDirs[0], new MoveState{
+				name = "LinkUpRight",
+				keys = new KeyCode[]{KeyCode.W, KeyCode.D},
+				direction = new Vector2(1,1).normalized,
+				flip = false
+			}},
+			//Up Left
+			{moveDirs[1], new MoveState{
+				name = "LinkUpRight",
+				keys = new KeyCode[]{KeyCode.W, KeyCode.A},
+				direction = new Vector2(-1,1).normalized,
+				flip = true
+			}},
+			//Down Right
+			{moveDirs[2], new MoveState{
+				name = "LinkDownLeft",
+				keys = new KeyCode[]{KeyCode.S, KeyCode.D},
+				direction = new Vector2(1,-1).normalized,
+				flip = true
+			}},
+			//Down Left
+			{moveDirs[3], new MoveState{
+				name = "LinkDownLeft",
+				keys = new KeyCode[]{KeyCode.S, KeyCode.A},
+				direction = new Vector2(-1,-1).normalized,
+				flip = false
+			}},
+			//Right
+			{moveDirs[4], new MoveState{
+				name = "Link Right",
+				keys = new KeyCode[]{KeyCode.D},
+				direction = Vector2.right,
+				flip = false
+			}},
+			//Left
+			{moveDirs[5], new MoveState{
+				name = "Link Right",
+				keys = new KeyCode[]{KeyCode.A},
+				direction = Vector2.left,
+				flip = true
+			}},
+			//Up
+			{moveDirs[6], new MoveState{
+				name = "Link Up",
+				keys = new KeyCode[]{KeyCode.W},
+				direction = Vector2.up,
+				flip = false
+			}},
+			//Down
+			{moveDirs[7], new MoveState{
+				name = "Link Down",
+				keys = new KeyCode[]{KeyCode.S},
+				direction = Vector2.down,
+				flip = false
+			}},
 		};
 
 	}
@@ -52,7 +111,10 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		Movement();
+		if (linkState == State.Idle || linkState == State.Moving)
+		{
+			Movement();
+		}
 	}
 
 	/*
@@ -61,76 +123,53 @@ public class PlayerController : MonoBehaviour
 	 */
 	void Movement()
 	{
-		Vector2 direction = Vector2.zero;
-		string currentAnimation = "";
-
+		MoveState moveState = new MoveState() { name = "" };
 		anim.speed = 1;
-		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+
+		//Check each possible direction
+		foreach (string dir in moveDirs)
 		{
-			anim.Play("LinkUpRight");
-			sr.flipX = false;
-			direction = directions["upRight"];
-			currentAnimation = "LinkUpRight";
+			bool found = true;
+			//Check keys required for each direction
+			foreach (KeyCode key in moveDict[dir].keys)
+			{
+				//keys not being pressed
+				if (!Input.GetKey(key))
+				{
+					found = false;
+				}
+			}
+			//if keys are being pressed for a certain direction
+			//set state to this direction and exit loop
+			if (found)
+			{
+				moveState = moveDict[dir];
+				break;
+			}
 		}
-		else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+		//Set animation if state was found
+		if (moveState.name != "")
 		{
-			anim.Play("LinkUpRight");
-			sr.flipX = true;
-			direction = directions["upLeft"];
-			currentAnimation = "LinkUpRight";
+
+			anim.Play(moveState.name);
+			sr.flipX = moveState.flip;
+
+			//Speed up if shift is being pressed
+			int finalSpeed = speed;
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				finalSpeed = speed + (speed / 2);
+			}
+
+			//Move player in state direction
+			transform.position += (Vector3)(moveState.direction * finalSpeed) * Time.deltaTime;
+			linkState = State.Moving;
 		}
-		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+		else // pause on current animation
 		{
-			anim.Play("LinkDownLeft");
-			sr.flipX = true;
-			direction = directions["downRight"];
-			currentAnimation = "LinkDownLeft";
-		}
-		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-		{
-			anim.Play("LinkDownLeft");
-			sr.flipX = false;
-			direction = directions["downLeft"];
-			currentAnimation = "LinkDownLeft";
-		}
-		else if (Input.GetKey(KeyCode.W))
-		{
-			anim.Play("Link Up");
-			direction = directions["up"];
-			currentAnimation = "Link Up";
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			sr.flipX = false;
-			anim.Play("Link Right");
-			direction = directions["right"];
-			currentAnimation = "Link Right";
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			anim.Play("Link Down");
-			direction = directions["down"];
-			currentAnimation = "Link Down";
-		}
-		else if (Input.GetKey(KeyCode.A))
-		{
-			sr.flipX = true;
-			anim.Play("Link Right");
-			direction = directions["left"];
-			currentAnimation = "Link Right";
-		}
-		else
-		{
-			anim.Play(currentAnimation, -1, 0f);
+			anim.Play(moveState.name, 0, 0f);
 			anim.speed = 0;
+			linkState = State.Idle;
 		}
-
-		int finalSpeed = speed;
-		if (Input.GetKey(KeyCode.LeftShift))
-		{
-			finalSpeed = speed + (speed / 2);
-		}
-
-		transform.position += (Vector3)(direction * finalSpeed) * Time.deltaTime;
 	}
 }
