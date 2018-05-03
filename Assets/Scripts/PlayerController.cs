9fa39f9bd8a07af4f61859ed6d2d222a2eb119f9
Daniel Brunelle <daniel.brunelle@me.com>
2018-05-03 14:30:02 -0600

    animations are hard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
 	float animspeed;
 
 	Dictionary<string, Vector2> directions;
+	Dictionary<string, int> movement;
 
 
 	// Use this for initialization
@@ -34,6 +35,18 @@ public class PlayerController : MonoBehaviour
 			{"downLeft", new Vector2(-1,-1).normalized},
 		};
 
+		movement = new Dictionary<string, int>()
+		{
+			{"up", 1},
+			{"down", 2},
+			{"left", 0},
+			{"right", 0},
+			{"upRight", 3},
+			{"upLeft", 3},
+			{"downRight",4},
+			{"downLeft", 4},
+		};
+
 	}
 
 	// Update is called once per frame
@@ -49,52 +62,67 @@ public class PlayerController : MonoBehaviour
 	void Movement()
 	{
 		Vector2 direction = Vector2.zero;
+		string currentAnimation = "";
 
+		anim.speed = 1;
 		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
 		{
 			anim.Play("LinkUpRight");
 			sr.flipX = false;
 			direction = directions["upRight"];
+			currentAnimation = "LinkUpRight";
 		}
 		else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
 		{
 			anim.Play("LinkUpRight");
 			sr.flipX = true;
 			direction = directions["upLeft"];
+			currentAnimation = "LinkUpRight";
 		}
 		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
 		{
 			anim.Play("LinkDownLeft");
 			sr.flipX = true;
 			direction = directions["downRight"];
+			currentAnimation = "LinkDownLeft";
 		}
 		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
 		{
 			anim.Play("LinkDownLeft");
 			sr.flipX = false;
 			direction = directions["downLeft"];
+			currentAnimation = "LinkDownLeft";
 		}
 		else if (Input.GetKey(KeyCode.W))
 		{
 			anim.Play("Link Up");
 			direction = directions["up"];
+			currentAnimation = "Link Up";
 		}
 		else if (Input.GetKey(KeyCode.D))
 		{
 			sr.flipX = false;
 			anim.Play("Link Right");
 			direction = directions["right"];
+			currentAnimation = "Link Right";
 		}
 		else if (Input.GetKey(KeyCode.S))
 		{
 			anim.Play("Link Down");
 			direction = directions["down"];
+			currentAnimation = "Link Down";
 		}
 		else if (Input.GetKey(KeyCode.A))
 		{
 			sr.flipX = true;
 			anim.Play("Link Right");
 			direction = directions["left"];
+			currentAnimation = "Link Right";
+		}
+		else
+		{
+			anim.Play(currentAnimation, -1, 0f);
+			anim.speed = 0;
 		}
 
 		int finalSpeed = speed;
