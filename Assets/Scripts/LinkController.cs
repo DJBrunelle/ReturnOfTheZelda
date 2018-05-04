430179d17de276c12a845e13c57c427d86e81fed
Daniel Brunelle <daniel.brunelle@me.com>
2018-05-03 19:25:01 -0600

    Added a seperate script to keep track of data used for links actions

---
+++ Assets/Scripts/LinkController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkController : MonoBehaviour
+{
+
+	public int speed;
+
+	private Animator anim;
+	private SpriteRenderer sr;
+	private float animspeed;
+	private enum State { Idle, Moving, Attacking, Frozen };
+	private State linkState;
+
+	// Use this for initialization
+	void Start()
+	{
+		linkState = State.Idle;
+		anim = GetComponent<Animator>();
+		sr = GetComponent<SpriteRenderer>();
+		anim.speed = 1;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (linkState == State.Idle || linkState == State.Moving)
+		{
+			Movement();
+		}
+	}
+
+	/*
+	 * Moves the player in direction based on WASD keys being held down
+	 * Shift makes link move faster
+	 */
+	void Movement()
+	{
+		LinkData.MoveState moveState = new LinkData.MoveState() { name = "" };
+
+		//Check each possible direction
+		foreach (string dir in LinkData.moveDirs)
+		{
+			bool found = true;
+			//Check keys required for each direction
+			foreach (KeyCode key in LinkData.moveDict[dir].keys)
+			{
+				//keys not being pressed
+				if (!Input.GetKey(key))
+				{
+					found = false;
+				}
+			}
+			//if keys are being pressed for a certain direction
+			//set state to this direction and exit loop
+			if (found)
+			{
+				moveState = LinkData.moveDict[dir];
+				break;
+			}
+		}
+		//Set animation if state was found
+		if (moveState.name != "")
+		{
+			anim.Play(moveState.name);
+			anim.SetInteger("Movement", moveState.paramMove);
+			sr.flipX = moveState.flip;
+
+			//Speed up if shift is being pressed
+			int finalSpeed = speed;
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+				finalSpeed = speed + (speed / 2);
+			}
+
+			//Move player in state direction
+			transform.position += (Vector3)(moveState.direction * finalSpeed) * Time.deltaTime;
+			linkState = State.Moving;
+		}
+		else // pause on current animation if keys not being pressed 
+		{
+			anim.SetInteger("Movement", -1);
+			linkState = State.Idle;
+		}
+	}
+}
---
+++ Assets/Scripts/LinkData.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkData : MonoBehaviour {
+
+	//State struct to represent the movement variables of each direction
+	public struct MoveState
+	{
+		public string name;
+		public string idle;
+		public int paramMove;
+		public KeyCode[] keys;
+		public Vector2 direction;
+		public bool flip;
+	}
+
+	/*
+	 * All possible abbreviated movements
+	 * ur = Up Right
+	 * ul = Up Left
+	 * dr = Down Right
+	 * dl = Down Left
+	 * rt = Right
+	 * lt = Left
+	 * up = Up
+	 * dn = Down
+	 */
+	public static string[] moveDirs = new string[] { "ur", "ul", "dr", "dl", "rt", "lt", "up", "dn" };
+
+	//Define states for each type of movement
+	public static Dictionary<string, MoveState> moveDict = new Dictionary<string, MoveState>(){
+			//Up Right
+			{moveDirs[0], new MoveState{
+				name = "LinkUpRight",
+				idle = "urIdle",
+				paramMove = 3,
+				keys = new KeyCode[]{KeyCode.W, KeyCode.D},
+				direction = new Vector2(1,1).normalized,
+				flip = false
+			}},
+			//Up Left
+			{moveDirs[1], new MoveState{
+				name = "LinkUpRight",
+				idle = "urIdle",
+				paramMove = 3,
+				keys = new KeyCode[]{KeyCode.W, KeyCode.A},
+				direction = new Vector2(-1,1).normalized,
+				flip = true
+			}},
+			//Down Right
+			{moveDirs[2], new MoveState{
+				name = "LinkDownLeft",
+				idle = "dlIdle",
+				paramMove = 4,
+				keys = new KeyCode[]{KeyCode.S, KeyCode.D},
+				direction = new Vector2(1,-1).normalized,
+				flip = true
+			}},
+			//Down Left
+			{moveDirs[3], new MoveState{
+				name = "LinkDownLeft",
+				idle = "dlIdle",
+				paramMove = 4,
+				keys = new KeyCode[]{KeyCode.S, KeyCode.A},
+				direction = new Vector2(-1,-1).normalized,
+				flip = false
+			}},
+			//Right
+			{moveDirs[4], new MoveState{
+				name = "Link Right",
+				idle = "rtIdle",
+				paramMove = 0,
+				keys = new KeyCode[]{KeyCode.D},
+				direction = Vector2.right,
+				flip = false
+			}},
+			//Left
+			{moveDirs[5], new MoveState{
+				name = "Link Right",
+				idle = "urIdle",
+				paramMove = 0,
+				keys = new KeyCode[]{KeyCode.A},
+				direction = Vector2.left,
+				flip = true
+			}},
+			//Up
+			{moveDirs[6], new MoveState{
+				name = "Link Up",
+				idle = "upIdle",
+				paramMove = 2,
+				keys = new KeyCode[]{KeyCode.W},
+				direction = Vector2.up,
+				flip = false
+			}},
+			//Down
+			{moveDirs[7], new MoveState{
+				name = "Link Down",
+				idle = "dnIdle",
+				paramMove = 1,
+				keys = new KeyCode[]{KeyCode.S},
+				direction = Vector2.down,
+				flip = false
+			}},
+		};
+}
