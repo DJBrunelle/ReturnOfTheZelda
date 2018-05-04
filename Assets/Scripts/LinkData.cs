83fd4a165542e282a28ee1a3c84faf116253698c
Daniel Brunelle <daniel.brunelle@me.com>
2018-05-03 23:38:13 -0600

    QOL and visible meta files

--- Assets/Scripts/LinkData.cs
+++ Assets/Scripts/LinkData.cs
@@ -31,77 +31,77 @@ public class LinkData : MonoBehaviour
 
 	//Define states for each type of movement
 	public static Dictionary<string, MoveState> moveDict = new Dictionary<string, MoveState>(){
-			//Up Right
-			{moveDirs[0], new MoveState{
-				name = "LinkUpRight",
-				idle = "urIdle",
-				paramMove = 3,
-				keys = new string[]{"Up", "Right"},
-				direction = new Vector2(1,1).normalized,
-				flip = false
-			}},
-			//Up Left
-			{moveDirs[1], new MoveState{
-				name = "LinkUpRight",
-				idle = "urIdle",
-				paramMove = 3,
-				keys = new string[]{"Up", "Left"},
-				direction = new Vector2(-1,1).normalized,
-				flip = true
-			}},
-			//Down Right
-			{moveDirs[2], new MoveState{
-				name = "LinkDownLeft",
-				idle = "dlIdle",
-				paramMove = 4,
-				keys = new string[]{"Down", "Right"},
-				direction = new Vector2(1,-1).normalized,
-				flip = true
-			}},
-			//Down Left
-			{moveDirs[3], new MoveState{
-				name = "LinkDownLeft",
-				idle = "dlIdle",
-				paramMove = 4,
-				keys = new string[]{"Down", "Left"},
-				direction = new Vector2(-1,-1).normalized,
-				flip = false
-			}},
-			//Right
-			{moveDirs[4], new MoveState{
-				name = "Link Right",
-				idle = "rtIdle",
-				paramMove = 0,
-				keys = new string[]{"Right"},
-				direction = Vector2.right,
-				flip = false
-			}},
-			//Left
-			{moveDirs[5], new MoveState{
-				name = "Link Right",
-				idle = "urIdle",
-				paramMove = 0,
-				keys = new string[]{"Left"},
-				direction = Vector2.left,
-				flip = true
-			}},
-			//Up
-			{moveDirs[6], new MoveState{
-				name = "Link Up",
-				idle = "upIdle",
-				paramMove = 2,
-				keys = new string[]{"Up"},
-				direction = Vector2.up,
-				flip = false
-			}},
-			//Down
-			{moveDirs[7], new MoveState{
-				name = "Link Down",
-				idle = "dnIdle",
-				paramMove = 1,
-				keys = new string[]{"Down"},
-				direction = Vector2.down,
-				flip = false
-			}},
-		};
+		//Up Right
+		{moveDirs[0], new MoveState{
+			name = "LinkUpRight",
+			idle = "urIdle",
+			paramMove = 3,
+			keys = new string[]{"Up", "Right"},
+			direction = new Vector2(1,1).normalized,
+			flip = false
+		}},
+		//Up Left
+		{moveDirs[1], new MoveState{
+			name = "LinkUpRight",
+			idle = "urIdle",
+			paramMove = 3,
+			keys = new string[]{"Up", "Left"},
+			direction = new Vector2(-1,1).normalized,
+			flip = true
+		}},
+		//Down Right
+		{moveDirs[2], new MoveState{
+			name = "LinkDownLeft",
+			idle = "dlIdle",
+			paramMove = 4,
+			keys = new string[]{"Down", "Right"},
+			direction = new Vector2(1,-1).normalized,
+			flip = true
+		}},
+		//Down Left
+		{moveDirs[3], new MoveState{
+			name = "LinkDownLeft",
+			idle = "dlIdle",
+			paramMove = 4,
+			keys = new string[]{"Down", "Left"},
+			direction = new Vector2(-1,-1).normalized,
+			flip = false
+		}},
+		//Right
+		{moveDirs[4], new MoveState{
+			name = "Link Right",
+			idle = "rtIdle",
+			paramMove = 0,
+			keys = new string[]{"Right"},
+			direction = Vector2.right,
+			flip = false
+		}},
+		//Left
+		{moveDirs[5], new MoveState{
+			name = "Link Right",
+			idle = "urIdle",
+			paramMove = 0,
+			keys = new string[]{"Left"},
+			direction = Vector2.left,
+			flip = true
+		}},
+		//Up
+		{moveDirs[6], new MoveState{
+			name = "Link Up",
+			idle = "upIdle",
+			paramMove = 2,
+			keys = new string[]{"Up"},
+			direction = Vector2.up,
+			flip = false
+		}},
+		//Down
+		{moveDirs[7], new MoveState{
+			name = "Link Down",
+			idle = "dnIdle",
+			paramMove = 1,
+			keys = new string[]{"Down"},
+			direction = Vector2.down,
+			flip = false
+		}},
+	};
 }
